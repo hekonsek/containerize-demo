46514773297d58c4e6083de3d668ca6c9ecfc3ba
Henryk Konsek <hekonsek@gmail.com>
2015-06-17 12:08:35 +0200

    Added commands pauses.

--- 1-cowsay/build.sh
+++ 1-cowsay/build.sh
@@ -1,3 +1,8 @@
+echo EXECUTING: docker build -t henry.k/cowsay:1.0 .
+read
 docker build -t henry.k/cowsay:1.0 .
+
+echo EXECUTING: docker run -t henry.k/cowsay:1.0 
+read
 docker run -t henry.k/cowsay:1.0 
 
